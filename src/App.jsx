3fa1c366abc57d077dd38abbe9b5261301e87896
PhungHao <phunghao2701@gmail.com>
2026-04-16 02:47:08 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import Dashboard from "./pages/Dashboard";
+import Rooms from "./pages/Rooms";
+
+import Security from "./pages/Security";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Dashboard />} />
+        <Route path="rooms" element={<Rooms />} />
+        <Route path="security" element={<Security />} />
+        {/* Route mặc định / 404 cho cài đặt */}
+        <Route
+          path="settings"
+          element={
+            <div className="p-10 font-bold text-xl">
+              Setting page coming soon...
+            </div>
+          }
+        />
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
